7e5636dea72a1df1bf66ade372445ef42017736a
Livbha <1176637849@qq.com>
2024-09-26 19:51:38 +0800

    Update OSS.cpp

--- src/OSS.cpp
+++ src/OSS.cpp
@@ -125,9 +125,9 @@ IntegerVector rcppOSS(NumericMatrix X, int n) {
 
     double t = 0;
     if (N > pow(n,2)) {
-      t = (N / (i+1)) * 10;
+      t = (N / (i+1)) * 5;
     } else {
-      t = (N / pow(i+1, r-1)) * 10;
+      t = (N / pow(i+1, r-1)) * 5;
     }
     // Rcout << "t = " << t << "\n";
     if (candi.length() > t) {
